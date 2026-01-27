c78f27790e92e7b09f16e13bc5c9ffb71771ccad
33antoinz <antoine.renault247@gmail.com>
2026-01-27 16:42:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.querySelectorAll('.module-card').forEach(card => {
-    card.addEventListener('click', () => {
-        alert("Accès réservé aux membres Privilège. Veuillez régulariser votre abonnement pour débloquer le protocole complet.");
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-});
+
